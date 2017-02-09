151f164dc465c255519cecbc672ddbfbd37563dc
Patrick <patrick.wong@uwaterloo.ca>
2017-02-09 17:03:56 -0500

    adding avatar

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # get 'users/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 #  get 'static_pages/home'
   get  '/help',    to: 'static_pages#help'
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'static_pages#home'
+
+  resources :users
 end
