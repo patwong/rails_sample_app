f99c16e2e4c2d37b7924ee4a45c69d5488ff218b
Patrick <patrick.wong@uwaterloo.ca>
2017-02-14 13:40:18 -0500

    checking if heroku img works

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     # debugger
+    # value of @user in /users/new is nil (exercise 7.1.3)
   end
 
   def show
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,18 @@
 module UsersHelper
 
   # Returns the Gravatar for the given user.
+
+  # getting images to display on development versus production
+  # http://stackoverflow.com/questions/38222989/gravatar-image-not-displaying
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    if Rails.env.production?
+      # puts 'hi'
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+    elsif Rails.env.development?
+      # puts 'bye'
+      gravatar_url = "https://gravatar.com/avatar/#{gravatar_id}"
+    end
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 end
