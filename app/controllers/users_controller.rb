55a4d5a5e7bf55d562ebc8fa0ce187d214455f3a
Patrick <patrick.wong@uwaterloo.ca>
2017-02-15 16:39:57 -0500

    adding error messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)   # not the final implementation
+    if @user.save
+      # handle a succesful save
+    else
+      render 'new'
+    end
+  end
+
   def show
     @user = User.find(params[:id])
     # debugger
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
+  # private section
 end
