36acce78f5b280000762c310bfbd224318d7c982
Patrick <patrick.wong@uwaterloo.ca>
2017-02-27 17:11:30 -0500

    at listing 8.2.1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     # debugger
-    # this should throw an exception but instead rails treats this as 'false' and moves to the else condition
+    # the second part of logical-AND is ignored since 'user' is nil - if-statement is evaluated to be false
+    # otherwise, an exception will be thrown and the if-statement fails
     if user && user.authenticate(params[:session][:password])
-      # log in the user
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,19 @@
 module SessionsHelper
+
+  # logs in the given user
+  def log_in(user)
+    # 'session' is a method distinct from the Sessions controller or session hash in sessions/new.html.erb
+    # it can be treated as a hash (Section 8.2.1)
+    session[:user_id] = user.id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  # returns true if the user is logged in, false otherwise
+  def logged_in?
+    !current_user.nil?
+  end
 end
