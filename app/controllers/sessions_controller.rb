f7aa9f4695c64afe5805eaaf6d2af6e10818e43f
Patrick <patrick.wong@uwaterloo.ca>
2017-02-27 15:59:11 -0500

    fix 'sessions' error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    # debugger
+    # this should throw an exception but instead rails treats this as 'false' and moves to the else condition
     if user && user.authenticate(params[:session][:password])
       # log in the user
     else
-      flash[:danger] = "Invalid emali/password combination"
+      flash.now[:danger] = "Invalid email/password combination"
       render 'new'
     end
   end
