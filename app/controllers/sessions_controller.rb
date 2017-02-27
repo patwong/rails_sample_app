47f64c02341ada51a26ad0c08fa5c9f884c88602
Patrick <patrick.wong@uwaterloo.ca>
2017-02-27 13:27:15 -0500

    adding warning flash to failed login

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # log in the user
+    else
+      flash[:danger] = "Invalid emali/password combination"
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
