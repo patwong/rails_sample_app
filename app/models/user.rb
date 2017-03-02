0c5b8f01564a43c3e7cdb8c9d2eedad89f99f85a
Patrick <patrick.wong@uwaterloo.ca>
2017-03-02 16:45:14 -0500

    working on adding cookie digest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   # validates :name, presence: true, length: {maximum: 50}
   # validates :email, presence: true, length: {maximum: 255}
 
@@ -17,9 +19,20 @@ class User < ApplicationRecord
   has_secure_password
   validates(:password, {presence: true, length: {minimum: 6}})
 
-  def User.digest(string)
+  # returns hash digest of the given string
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
         BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  # returns a random new token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
