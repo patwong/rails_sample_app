f27e1b680582e782594e1a711270dd4e0282daf5
Patrick <patrick.wong@uwaterloo.ca>
2017-03-06 16:05:23 -0500

    testing current_user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,15 +3,17 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    # by turning 'user' into an instance variable (@user), we can use the method 'assigns'
+    # in an integration test to check @user's attributes (exercise 9.3.1.1)
+    @user = User.find_by(email: params[:session][:email].downcase)
     # debugger
     # the second part of logical-AND is ignored since 'user' is nil - if-statement is evaluated to be false
     # otherwise, an exception will be thrown and the if-statement fails
     # i.e. shortcircuiting
-    if user && user.authenticate(params[:session][:password])
-      log_in user           # parenthesis not needed for method calls
-      remember user
-      redirect_to user
+    if @user && @user.authenticate(params[:session][:password])
+      log_in @user           # parenthesis not needed for method calls
+      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
