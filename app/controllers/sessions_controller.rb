9b93504889605defb2e643ebae03d038985cd7f7
Patrick <patrick.wong@uwaterloo.ca>
2017-03-30 17:40:32 -0400

    modify session to login only if activated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,10 +15,17 @@ class SessionsController < ApplicationController
     # otherwise, an exception will be thrown and the if-statement fails
     # i.e. shortcircuiting
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user           # parenthesis not needed for method calls
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      # redirect_to @user
-      redirect_back_or @user
+      if user.activated?        # ch11
+        log_in @user           # parenthesis not needed for method calls
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        # redirect_to @user
+        redirect_back_or @user
+      else
+        message = "Account not activated. "
+        message += "Check your email for the activation link."
+        flash[:warning] = message
+        redirect_to root_path
+      end
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
